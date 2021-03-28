864a4ca88bb801cb46a9fe79d87a447db1e35f14
Matthew stibbins <matthewstibbins@Matthews-MacBook-Air.local>
2021-03-28 00:04:15 -0700

    updated example

---
+++ Example/src/main/java/Example.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+
+public class Example {
+  public static void main(String[] args) {
+    Map<String, String> headers = new HashMap<>();
+    String url = "https://youtube.com";
+    String response = get(url, headers);
+    System.out.println(response);
+  }
+
+
+  public static String get(String url, Map<String, String> headers) {
+    try {
+      ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+
+        @Override
+        public String handleResponse(final HttpResponse response) throws
+            ClientProtocolException, IOException {
+
+          int status = response.getStatusLine().getStatusCode();
+          if (status >= 200 && status < 300) {
+            HttpEntity entity = response.getEntity();
+            return entity != null ? EntityUtils.toString(entity) : null;
+          }
+          return null;
+        }
+      };
+
+      CloseableHttpClient httpClient = HttpClients.createDefault();
+      HttpGet httpGet = new HttpGet(url);
+      for (String key : headers.keySet()) {
+        httpGet.addHeader(key, headers.get(key));
+      }
+      String response = httpClient.execute(httpGet, responseHandler);
+      return response;
+
+    }catch (final Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+}
